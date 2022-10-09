f1c0be03e9c74da4705a443f25e6364f421b6e78
“nhatlh91” <nhatlg@gmail.com>
2022-10-09 08:49:20 +0700

    Module2

---
+++ module2/src/ss01_java_overal/thuc_hanh/SystemTime.java
@@ -0,0 +1,8 @@
+package ss01_java_overal.thuc_hanh;
+import java.util.Date;
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
