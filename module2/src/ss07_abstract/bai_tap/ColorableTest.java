d5acc5d46d8b8f5750270dd7bb6526679fc67beb
“nhatlh91” <nhatlg@gmail.com>
2022-10-25 22:11:12 +0700

    CleanCode

--- module2/src/ss07_abstract/bai_tap/ColorableTest.java
+++ module2/src/ss07_abstract/bai_tap/ColorableTest.java
@@ -2,7 +2,6 @@ package ss07_abstract.bai_tap;
 
 public class ColorableTest {
     public static void main(String[] args) {
-        int i = 0;
         Shape[] shapes = new Shape[5];
         shapes[0] = new Circle(1.7);
         shapes[1] = new Square(5.4);
@@ -10,10 +9,9 @@ public class ColorableTest {
         shapes[3] = new Circle();
         shapes[4] = new Square();
 
-        for( Shape item: shapes) {
-            System.out.println("Diện tích hình thứ"+i+" là: "+item.getArea());
-            i++;
-            if (item instanceof Square) ((Square) item).howToColor();
+        for (int i = 0; i < shapes.length; i++) {
+            System.out.println("Diện tích hình thứ " + i + 1 + " là: " + shapes[i].getArea());
+            if (shapes[i] instanceof Square) ((Square) shapes[i]).howToColor();
         }
     }
 }
