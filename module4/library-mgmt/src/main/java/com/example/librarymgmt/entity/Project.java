3adcbc0afa8e92748ceead79dfdbeb470edfe615
Brian.Le <brian.le@corp.baimeihome.com>
2023-06-21 09:03:14 +0700

    project_beginning

--- module4/library-mgmt/src/main/java/com/example/librarymgmt/entity/Project.java
+++ module4/library-mgmt/src/main/java/com/example/librarymgmt/entity/Project.java
@@ -20,30 +20,28 @@ public class Project {
     private String projectCode;
     @NotEmpty(message = "Tên dự án không thể trống")
     private String projectName;
-    @Min(value = 30000000,message = "Kinh phí không thể dưới 30 triệu")
-    private int projectCost;
+    @Min(value = 30000000, message = "Kinh phí không thể dưới 30 triệu")
+    private double projectCost;
     @Column(name = "project_desc", length = 2000)
     @NotEmpty
     private String projectDesc;
     private Date announDate;
-    @Min(value = 2,message = "Thời gian đăng ký không bé hơn 2 tháng")
-    @Max(value = 12,message = "Thời gian đăng ký không lâu hơn 12 tháng")
+    @Min(value = 2, message = "Thời gian đăng ký không bé hơn 2 tháng")
+    @Max(value = 12, message = "Thời gian đăng ký không lâu hơn 12 tháng")
     private int announMonth;
-    private int fee;
+    private double fee;
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "company_id")
     private Company company;
 
-    public void setFee(){
-    	this.fee = projectCost<100000000?projectCost*0.01*announMonth:projectCost<=500000000?projectCost*0.02*announMonth:projectCost*0.03*announMonth;
-
-
-        if(projectCost<100000000){
-            this.fee = (int) (projectCost*0.01*announMonth);
-        } else if (projectCost<=500000000){
-            this.fee = (int) (projectCost*0.02*announMonth);
-        } else {
-            this.fee = (int) (projectCost*0.03*announMonth);
-        }
+    public void setFee() {
+        this.fee = projectCost < 100000000.0 ? projectCost * 0.01 * announMonth : projectCost <= 500000000 ? projectCost * 0.02 * announMonth : projectCost * 0.03 * announMonth;
+//        if (projectCost < 100000000) {
+//            this.fee = (int) (projectCost * 0.01 * announMonth);
+//        } else if (projectCost <= 500000000) {
+//            this.fee = (int) (projectCost * 0.02 * announMonth);
+//        } else {
+//            this.fee = (int) (projectCost * 0.03 * announMonth);
+//        }
     }
 }
